f0bcc8474f4f0b4b2a2c31af48e5c81c64b0b6ec
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-01 19:53:44 -0500

    removed mypy

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -17,13 +17,6 @@ return {
                 null_ls.builtins.diagnostics.ruff.with({
                     filetypes = { "python" },
                 }),
-                null_ls.builtins.diagnostics.mypy.with({
-                    filetypes = { "python" },
-                    extra_args = function()
-                        local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
-                        return { "--python-executable", virtual .. "/bin/python3" }
-                    end,
-                }),
                 null_ls.builtins.completion.luasnip,
                 null_ls.builtins.completion.spell,
             },
