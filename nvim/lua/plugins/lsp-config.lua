f9ca8b6a841b3d80e6413c912e7b56488417ed14
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-05 21:09:14 -0500

    pyright is dumb

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,90 +1,84 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-            vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "clangd",
-                    "dockerls",
-                    "golangci_lint_ls",
-                    "lua_ls",
-                    "pyright",
-                    "rust_analyzer",
-                    "tsserver",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+			vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"clangd",
+					"dockerls",
+					"golangci_lint_ls",
+					"lua_ls",
+					"pyright",
+					"rust_analyzer",
+					"tsserver",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.dockerls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.golangci_lint_ls.setup({})
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                settings = {
-                    pyright = {
-                        disableLanguageServices = true,
-                        disableOrganizeImports = true,
-                    },
-                    python = {
-                        analysis = {
-                            logLevel = "information",
-                            autoImportCompletions = false,
-                            diagnosticMode = "openFilesOnly",
-                            typeCheckingMode = "basic",
-                            diagnosticSeverityOverrides = {
-                                strictParameterNoneValue = "information",
-                                reportInconsistentOverload = false,
-                                reportPossiblyUnboundVariable = "information",
-                                reportIncompatibleMethodOverride = false,
-                                reportIncompatibleVariableOverride = false,
-                                reportOverlappingOverload = false,
-                                reportImplicitOverride = false,
-                                reportUnknownArgumentType = false,
-                                reportUnknownMemberType = false,
-                                reportArgumentType = false,
-                                reportInvalidTypeArguments = false,
-                            }, -- specify overrides here
-                        },
-                    },
-                },
-            })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.golangci_lint_ls.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				settings = {
+					pyright = {
+						disableLanguageServices = false,
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							logLevel = "information",
+							autoImportCompletions = false,
+							diagnosticMode = "openFilesOnly",
+							typeCheckingMode = "standard",
+							diagnosticSeverityOverrides = {
+								reportInconsistentOverload = false,
+								reportIncompatibleMethodOverride = false,
+								reportIncompatibleVariableOverride = false,
+								reportOverlappingOverload = false,
+								reportImplicitOverride = false,
+							},
+						},
+					},
+				},
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
