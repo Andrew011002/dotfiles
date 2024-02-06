01bdeb918e30148132c150661c15fc214feb896f
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-05 20:01:57 -0500

    pyright updates

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,75 +1,80 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-			vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"clangd",
-					"dockerls",
-					"golangci_lint_ls",
-					"lua_ls",
-					"pyright",
-					"rust_analyzer",
-					"tsserver",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+            vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "clangd",
+                    "dockerls",
+                    "golangci_lint_ls",
+                    "lua_ls",
+                    "pyright",
+                    "rust_analyzer",
+                    "tsserver",
+                },
             })
-			lspconfig.golangci_lint_ls.setup({})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.golangci_lint_ls.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
                 settings = {
                     python = {
                         analysis = {
                             autoImportCompletions = false,
                             diagnosticMode = "openFilesOnly",
-                            diagnosticSeverityOvverrides = {
-                                reportGeneralTypeIssues = "false",
-                                reportFunctionMemberAccess = "false",
-                            }
-                        }
-                    }
-                }
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
+                            diagnosticSeverityOverrides = {
+                                --         reportGeneralTypeIssues = "false",
+                                reportIncompatibleMethodOverride = "false",
+                                reportImplicitOverride = "false",
+                                reportUnknownArgumentType = "false",
+                                reportUnknownMemberType = "false",
+                                reportArgumentType = "false",
+                                reportInvalidTypeArguments = "false",
+                            },
+                        },
+                    },
+                },
             })
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
