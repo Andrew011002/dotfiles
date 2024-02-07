36b8e295e16dc54724fa462a3d801b4a98da6f04
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-06 20:24:15 -0500

    dotfile update

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -8,11 +8,25 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
 -- bindings
+
+-- tabbing
 vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-d>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ">>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
+-- window nav
 vim.api.nvim_set_keymap("n", "<leader>h", "<C-w>h", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>j", "<C-w>j", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>k", "<C-w>k", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>l", "<C-w>l", { noremap = true, silent = true })
+
+-- pane nav
 vim.api.nvim_set_keymap("n", "gt", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "gT", ":bprevious<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>q", ":bd<CR>", { noremap = true, silent = true })
+
+-- comments
+vim.api.nvim_set_keymap("n", "<C-/>", "gcc", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-/>", "<Esc>gccA", { noremap = true, silent = true })
