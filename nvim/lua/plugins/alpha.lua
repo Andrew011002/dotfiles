e6959b49c51093564d0fa64c939a6f701a4daf88
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-15 19:01:53 -0500

    update to conf

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -16,12 +16,12 @@ return {
 ]]
 			dashboard.section.header.val = vim.split(logo, "\n")
 			dashboard.section.buttons.val = {
-				dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-				dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-				dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-				dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+				dashboard.button("ff", " " .. " Find file", ":Telescope find_files <CR>"),
+				dashboard.button("fr", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("fg", " " .. " Find text", ":Telescope live_grep <CR>"),
+				-- dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+				dashboard.button("L", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+				dashboard.button("qa", " " .. " Quit", ":qa<CR>"),
 			}
 			dashboard.section.header.opts.hl = "AlphaHeader"
 			dashboard.opts.layout[1].val = 6
