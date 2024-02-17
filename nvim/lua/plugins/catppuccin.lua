3615f64b025d4affd886bca0ec4620a00617833d
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-16 21:19:12 -0500

    update for nvim

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,28 +1,34 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			term_colors = true,
-			transparent_background = false,
-			no_italic = true,
-			no_bold = false,
-			styles = {
-				comments = {},
-				conditionals = {},
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-			},
-		})
-
-		vim.cmd.colorscheme("catppuccin")
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+        local mocha_palette = require("catppuccin.palettes").get_palette("mocha")
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            term_colors = true,
+            transparent_background = false,
+            no_italic = true,
+            no_bold = false,
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            color_overrides = {
+                mocha = {
+                    base = mocha_palette.crust,
+                    mantle = mocha_palette.mantle,
+                },
+            },
+        })
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
