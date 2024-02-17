826280e441182a93bbdc968692b7e2008cb34035
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-17 09:56:23 -0500

    cond to mauve

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,56 +1,57 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		local mocha_palette = require("catppuccin.palettes").get_palette("mocha")
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			term_colors = true,
-			transparent_background = true,
-			no_italic = true,
-			no_bold = false,
-			custom_highlights = function()
-				return {
-					["@function.method"] = { fg = mocha_palette.blue },
-					["@comment"] = { fg = mocha_palette.base },
-					["@constructor"] = { fg = mocha_palette.blue },
-					["@attribute"] = { fg = mocha_palette.blue },
-					["@variable.builtin"] = { fg = mocha_palette.yellow },
-					["@variable"] = { fg = mocha_palette.text },
-					["@variable.parameter"] = { fg = mocha_palette.maroon },
-					["@punctuation.special"] = { fg = mocha_palette.overlay2 },
-					["@operator"] = { fg = mocha_palette.overlay2 },
-					["@character.special"] = { fg = mocha_palette.yellow },
-					["@keyword"] = { fg = mocha_palette.mauve },
-					["@keyword.exception"] = { fg = mocha_palette.mauve },
-					["@keyword.storage"] = { fg = mocha_palette.mauve },
-					["@keyword.import"] = { fg = mocha_palette.mauve },
-					["@keyword.return"] = { fg = mocha_palette.mauve },
-					["@keyword.function"] = { fg = mocha_palette.mauve },
-					["@keyword.operator"] = { fg = mocha_palette.mauve },
-					["@keyword.repeat"] = { fg = mocha_palette.mauve },
-				}
-			end,
-			styles = {
-				comments = {},
-				conditionals = {},
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-			},
-			color_overrides = {
-				mocha = {
-					base = mocha_palette.crust,
-					mauve = mocha_palette.text, -- so that alphanivm shows text
-				},
-			},
-		})
-		vim.cmd.colorscheme("catppuccin")
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+        local mocha_palette = require("catppuccin.palettes").get_palette("mocha")
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            term_colors = true,
+            transparent_background = true,
+            no_italic = true,
+            no_bold = false,
+            custom_highlights = function()
+                return {
+                    ["@function.method"] = { fg = mocha_palette.blue },
+                    ["@comment"] = { fg = mocha_palette.base },
+                    ["@constructor"] = { fg = mocha_palette.blue },
+                    ["@attribute"] = { fg = mocha_palette.blue },
+                    ["@variable.builtin"] = { fg = mocha_palette.yellow },
+                    ["@variable"] = { fg = mocha_palette.text },
+                    ["@variable.parameter"] = { fg = mocha_palette.maroon },
+                    ["@punctuation.special"] = { fg = mocha_palette.overlay2 },
+                    ["@operator"] = { fg = mocha_palette.overlay2 },
+                    ["@character.special"] = { fg = mocha_palette.yellow },
+                    ["@keyword"] = { fg = mocha_palette.mauve },
+                    ["@keyword.exception"] = { fg = mocha_palette.mauve },
+                    ["@keyword.storage"] = { fg = mocha_palette.mauve },
+                    ["@keyword.import"] = { fg = mocha_palette.mauve },
+                    ["@keyword.return"] = { fg = mocha_palette.mauve },
+                    ["@keyword.function"] = { fg = mocha_palette.mauve },
+                    ["@keyword.operator"] = { fg = mocha_palette.mauve },
+                    ["@keyword.repeat"] = { fg = mocha_palette.mauve },
+                    ["@keyword.conditional"] = { fg = mocha_palette.mauve },
+                }
+            end,
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            color_overrides = {
+                mocha = {
+                    base = mocha_palette.crust,
+                    mauve = mocha_palette.text, -- so that alphanivm shows text
+                },
+            },
+        })
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
