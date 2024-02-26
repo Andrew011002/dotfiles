37c00687763c17727c053c80a7f9267848e82819
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-26 18:59:55 -0500

    local llm

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+	"David-Kunz/gen.nvim",
+	config = function()
+		require("gen").setup({
+            model = "mistral",
+            show_model = true,
+            show_prompt = true,
+			vim.keymap.set({ "n", "v" }, "<leader>]", ":Gen<CR>"),
+		})
+	end,
+}
