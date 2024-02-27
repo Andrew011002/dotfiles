4e3f66f97f0cf84254edc0537aa8cba7ce51c8f7
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-26 19:02:55 -0500

    use gemma

--- nvim/lua/plugins/gen.lua
+++ nvim/lua/plugins/gen.lua
@@ -2,7 +2,7 @@ return {
 	"David-Kunz/gen.nvim",
 	config = function()
 		require("gen").setup({
-            model = "mistral",
+            model = "gemma",
             show_model = true,
             show_prompt = true,
 			vim.keymap.set({ "n", "v" }, "<leader>]", ":Gen<CR>"),
