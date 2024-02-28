2e6723bf9caed3b0f6d3270e48c64a63aff1c40b
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-27 20:13:04 -0500

    going to try not to use neotree or buffline

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,9 +1,9 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-	config = function()
-		vim.keymap.set("n", "<leader>n", ":Neotree toggle left <CR>", {})
-		vim.keymap.set("n", "<leader><ESC>", ":Neotree close left <CR>", {})
-	end,
+-- 	"nvim-neo-tree/neo-tree.nvim",
+-- 	branch = "v3.x",
+-- 	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+-- 	config = function()
+-- 		vim.keymap.set("n", "<leader>n", ":Neotree toggle left <CR>", {})
+-- 		vim.keymap.set("n", "<leader><ESC>", ":Neotree close left <CR>", {})
+-- 	end,
 }
