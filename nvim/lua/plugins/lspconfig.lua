9316821c7bbb065166084c7b09ae6bca4705dfe3
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-09 16:39:13 -0500

    using tool to make sure lsps are up to date

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,28 +1,28 @@
 return {
-    { 
-    "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
 		config = function()
 			require("mason").setup({})
 			vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
-		end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
+
+            require("mason-tool-installer").setup({
                 ensure_installed = {
                     "clangd", 
                     "cssls", 
                     "gopls",
                     "html",
                     "htmx",
+                    "lua_ls",
                     "pyright",
-                }
+                },
+                auto_update = true,
+                debounce_hours = 3,
             })
-        end,
 
-    },
-    {
-        "neovim/nvim-lspconfig"
-    },
+            local lspconfig = require('lspconfig')
+		end,
 }
