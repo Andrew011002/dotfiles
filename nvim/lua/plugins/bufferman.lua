f6dbb5a5835caea0bd7157c176c71d579aae193a
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-09 14:14:56 -0500

    nvim startover

--- nvim/lua/plugins/bufferman.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"j-morano/buffer_manager.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("buffer_manager").setup({})
-		local ui = require("buffer_manager.ui")
-        vim.keymap.set("n", "<CR>", ui.toggle_quick_menu, {})
-	end,
-}
