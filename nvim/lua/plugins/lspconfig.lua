aaf4afc957eb8f087d3c9eb18d83d3537e2b9787
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-09 18:56:32 -0500

    update to lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -20,9 +20,25 @@ return {
                     "pyright",
                 },
                 auto_update = true,
-                debounce_hours = 3,
             })
 
             local lspconfig = require('lspconfig')
+            local builtin =  require("telescope.builtin")
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+              group = vim.api.nvim_create_augroup('LspBootUp', {clear = true}),
+              callback = function(event)
+                vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                local opts = { buffer = event.buf }
+            
+                vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, {opts, desc = "[G]oto [D]efinition"})
+                vim.keymap.set("n", "gr", builtin.lsp_references, {opts, desc = "[G]oto [R]eferences"})
+                vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {opts, desc = "[D]ocument [S]ymbols"})
+                vim.keymap.set("n", "<leader>ws", builtin.lsp_dynamic_workspace_symbols, {opts, desc = "[W]ork [S]pace Symbols"})
+                vim.keymap.set("n", "<leader>gI", builtin.lsp_implementations, {opts, desc = "[G]oto Implementations"})
+              end,
+            })
+
+--            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		end,
 }
