d96279c811fa64af8322bcddd85b90d9c6c2d5cb
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-10 18:17:46 -0400

    forgot to call setup for TS

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,23 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = "<cmd>TSUpdate",
-    config = function()
-        ensure_installed = {
-            "c",
-            "cpp",
-            "css",
-            "go",
-            "html",
-            "lua",
-            "luadoc",
-            "python",
-            "vim",
-            "vimdoc",
-        }
-        highlight = {enable = true}
-        indent = { enable = true}
-        auto_install = true
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = "<cmd>TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"css",
+				"go",
+				"html",
+				"lua",
+				"luadoc",
+				"python",
+				"vim",
+				"vimdoc",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+		})
+	end,
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -60,7 +60,8 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
+-- diagnostics
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>qf", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uick[F]ix list" })
