515aad20af24c43b63d9bcfbca60b6d7ce3afdec
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-10 17:54:31 -0400

    autocomplete

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,72 +1,155 @@
 return {
-        {"neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-        },
+	{ -- LSP Config & Setup
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
 		config = function()
 			require("mason").setup({})
 			vim.keymap.set("n", "<leader>M", ":Mason <CR>", {})
 
-            -- lsp bindings
-            local builtin =  require("telescope.builtin")
-            vim.api.nvim_create_autocmd('LspAttach', {
-              group = vim.api.nvim_create_augroup('LspBootUp', {clear = true}),
-              callback = function(event)
-                vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+			local builtin = require("telescope.builtin")
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("LspBootUp", { clear = true }),
+				callback = function(event)
+					vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = event.buf, desc = "[G]oto [D]ecleration"})
-                vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = event.buf, desc = "[G]oto [D]efinition"})
-                vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = event.buf, desc = "[G]oto [R]eferences"})
-                vim.keymap.set("n", "gI", builtin.lsp_implementations, { buffer = event.buf, desc = "[G]oto [I]mplementations"})
-                vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { buffer = event.buf, desc = "[D]ocument [S]ymbols"})
-                vim.keymap.set("n", "<leader>ws", builtin.lsp_workspace_symbols, { buffer = event.buf, desc = "[W]orkspace [S]ymbols"})
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, desc = "[R]e[N]ame"})
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = event.buf, desc = "[C]ode [A]ctions"})
-                vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { buffer = event.buf, desc = "Hover"})
-              end,
-            })
+					vim.keymap.set(
+						"n",
+						"gD",
+						vim.lsp.buf.declaration,
+						{ buffer = event.buf, desc = "[G]oto [D]ecleration" }
+					)
+					vim.keymap.set(
+						"n",
+						"gd",
+						builtin.lsp_definitions,
+						{ buffer = event.buf, desc = "[G]oto [D]efinition" }
+					)
+					vim.keymap.set(
+						"n",
+						"gr",
+						builtin.lsp_references,
+						{ buffer = event.buf, desc = "[G]oto [R]eferences" }
+					)
+					vim.keymap.set(
+						"n",
+						"gI",
+						builtin.lsp_implementations,
+						{ buffer = event.buf, desc = "[G]oto [I]mplementations" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>ds",
+						builtin.lsp_document_symbols,
+						{ buffer = event.buf, desc = "[D]ocument [S]ymbols" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>ws",
+						builtin.lsp_workspace_symbols,
+						{ buffer = event.buf, desc = "[W]orkspace [S]ymbols" }
+					)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = event.buf, desc = "[R]e[N]ame" })
+					vim.keymap.set(
+						"n",
+						"<leader>ca",
+						vim.lsp.buf.code_action,
+						{ buffer = event.buf, desc = "[C]ode [A]ctions" }
+					)
+					vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { buffer = event.buf, desc = "Hover" })
+				end,
+			})
 
+			local servers = {
+				clangd = {},
+				cssls = {},
+				gopls = {},
+				html = {},
+				htmx = {},
+				lua_ls = {},
+				pyright = {},
+				stylua = {},
+			}
 
-            -- language server config and setup
-            local servers = { clangd = {}, 
-                                cssls = {}, 
-                                gopls = {}, 
-                                html = {}, 
-                                htmx = {}, 
-                                lua_ls = {}, 
-                                pyright = {}, 
-                                stylua = {}, 
-                            }
+			local ensure_installed = vim.tbl_keys(servers or {})
+			require("mason-tool-installer").setup({
+				ensure_installed = ensure_installed,
+				auto_update = true,
+			})
 
-            local ensure_installed = vim.tbl_keys(servers or {})
-            require('mason-tool-installer').setup { 
-                                    ensure_installed = ensure_installed,
-                                    auto_update = true
-            }
-
-            local lspconfig = require('lspconfig')
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            require('mason-lspconfig').setup {
-                    handlers = {
-                      function(server_name)
-                        local server = servers[server_name] or {}
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        lspconfig[server_name].setup(server)
-                      end,
-                    },
-                  }
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						lspconfig[server_name].setup(server)
+					end,
+				},
+			})
 		end,
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip"
-        },
-        config = function()
-        end,
-    }
+	},
+
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				notify_on_error = false,
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+				formatters_by_ft = {
+					go = { "gofmt" },
+					lua = { "stylua" },
+					python = { "black" },
+				},
+			})
+		end,
+	},
+
+	{ -- Autocomple
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<C-l>"] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { "i", "s" }),
+					["<C-h>"] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -55,7 +55,12 @@ vim.keymap.set("n", "gt", "<cmd>bnext<CR>")
 vim.keymap.set("n", "gT", "<cmd>bprevious<CR>")
 
 -- enforce the learning curve!
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
