9e6533b32a39bfe695c66a2ce5c726c14583f5a2
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-11 11:57:09 -0400

    not sure how tog et it to work yet

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,23 @@
 return {
-	"catppuccin/nvim",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
-			no_italic = true,
-			no_bold = true,
-			no_underline = true,
-		})
-		-- vim.cmd.colorscheme("catppuccin")
-	end,
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				no_italic = true,
+				no_bold = true,
+				no_underline = true,
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	-- {
+	-- 	"sontungexpt/witch",
+	-- 	priority = 0,
+	-- 	lazy = false,
+	-- 	config = function(_, opts)
+	-- 		require("witch").setup(opts)
+	-- 	end,
+	-- },
 }
