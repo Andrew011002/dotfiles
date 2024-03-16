dae1fdff402b7be1537c491d07e8493fb3df3bf9
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-16 18:57:14 -0400

    switched model

--- nvim/lua/plugins/ollama.lua
+++ nvim/lua/plugins/ollama.lua
@@ -2,7 +2,7 @@ return {
 	"David-Kunz/gen.nvim",
 	config = function()
 		require("gen").setup({
-			model = "mixtral", -- The default model to use.
+			model = "gemma", -- The default model to use.
 			display_mode = "float", -- The display mode. Can be "float" or "split".
 			show_prompt = false, -- Shows the Prompt submitted to Ollama.
 			show_model = true, -- Displays which model you are using at the beginning of your chat session.
