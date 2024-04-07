8f2ea261d99da4bafa51940ea9a1e4e7e3b077de
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-07 14:07:27 -0400

    update to conf

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -7,32 +7,7 @@ return {
 	},
 	config = function()
 		require("codeium").setup({})
-		vim.keymap.set("n", "<leader>ce", function()
-			vim.g.codeium_enabled = true
-		end, { silent = true })
-		vim.keymap.set("n", "<leader>cd", function()
-			vim.g.codeium_enabled = false
-		end, { silent = true })
-
-		vim.keymap.set("n", "<leader>cg", function()
-			if vim.g.codeium_render == nil or vim.g.codeium_render == false then
-				vim.g.codeium_render = true
-			else
-				vim.g.codeium_render = false
-			end
-		end, { silent = true })
-
-		vim.keymap.set("i", "<Tab>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-]>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-[>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-]>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true, silent = true })
+		vim.keymap.set("n", "<leader>ce", "<cmd>Codeium Enable<cr>", { desc = "[C]odeium [E]nable" })
+		vim.keymap.set("n", "<leader>cd", "<cmd>Codeium Disable<cr>", { desc = "[C]odeium [D]isable" })
 	end,
 }
