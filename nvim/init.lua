03a9c4ad9736bc538f4b50769d80efd7f473b3b1
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-12 12:52:39 -0400

    potential fix for wsl copy paste

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,5 +15,21 @@ vim.opt.rtp:prepend(lazypath)
 vim.keymap.set("n", "<leader>L", ":Lazy <CR>", {})
 local opts = {}
 
+-- copy paste for WSL
+local clip = "/mnt/c/Windows/System32/clip.exe"
+if vim.fn.executable(clip) then
+	opts = {
+		callback = function()
+			if vim.v.event.operator ~= "y" then
+				return
+			end
+			vim.fn.system(clip, vim.fn.getreg(0))
+		end,
+	}
+
+	opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+	vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
+end
+
 require("vim-options")
 require("lazy").setup("plugins", opts)
