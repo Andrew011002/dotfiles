8f3098b9e8b64be4a1c00646cc9413cd604387ae
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-09 19:36:06 -0400

    autopairs fix

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -211,6 +211,9 @@ return {
 	{ -- Autopairs
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
 	},
 	{ -- Comment
 		"terrortylor/nvim-comment",
