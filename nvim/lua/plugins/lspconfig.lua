ea8676bb26009e6c28f51cc17f505d51c07ced07
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-09 19:20:40 -0400

    ll and remove tmux (don't use)

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -111,7 +111,7 @@ return {
 			require("conform").setup({
 				notify_on_error = false,
 				format_on_save = {
-					timeout_ms = 500,
+					timeout_ms = 250,
 					lsp_fallback = true,
 				},
 				formatters_by_ft = {
