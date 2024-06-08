c2f06be4907be8c1f1b5508098dd27ea8973d901
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-08 16:01:40 -0400

    it's a statusline

---
+++ nvim/lua/plugins/statusline.lua
@@ -0,0 +1,26 @@
+return {
+	"sontungexpt/sttusline",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = { "BufEnter" },
+	config = function(_, opts)
+		require("sttusline").setup({
+			statusline_color = "StatusLine",
+			laststatus = 3,
+			components = {
+				"mode",
+				"filename",
+				"git-branch",
+				"git-diff",
+				"%=",
+				"diagnostics",
+				"lsps-formatters",
+				"indent",
+				"encoding",
+				"pos-cursor",
+				"pos-cursor-progress",
+			},
+		})
+	end,
+}
