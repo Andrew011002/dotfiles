2102f261a942838b39630cf964928732a4125614
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-19 17:05:57 -0400

    make dirs first

--- install.zsh
+++ install.zsh
@@ -2,6 +2,9 @@
 
 dotfiles_path="$1"
 
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/starship
+
 ln -sf "$dotfiles_path/nvim" ~/.config/nvim
 ln -sf "$dotfiles_path/tmux/.tmux.conf" ~/.tmux.conf
 ln -sf "$dotfiles_path/zsh/.zshrc" ~/.zshrc
